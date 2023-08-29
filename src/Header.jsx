eea62a54e5c174a513d7ffe6639d304907a09df8
navin <navin@thoghtwin.com>
2023-08-29 14:56:44 +0530

    Fixed toast message

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,19 @@
-import React, { useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Header = (props) => {
-    const notify = () => toast.success("Wow so easy!");
-    
-    useEffect(() => {
-        if(props?.value){
-            notify();
-        }
-    }, [props]);
-    
-    return (
-        <div className='p-5 bg-blue-500 text-white text-3xl font-bold'>
-            Header Micro-Fronted |
-            <ToastContainer />
-        </div>
-    );
-}
+  const notify = () => toast.success("Welcome user!");
+
+  useEffect(() => {
+    notify();
+  }, [props?.value]);
+
+  return (
+    <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+      Header Micro-Fronted |
+    </div>
+  );
+};
 
 export default Header;
