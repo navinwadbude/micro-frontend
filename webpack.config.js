c6e82dc6fe1f973a9cbab3ea6694c01922a7c4e2
navin <navin@thoghtwin.com>
2023-08-29 15:46:27 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "/",
+    publicPath: "https://timely-stardust-0ecb5b.netlify.app/",
   },
 
   resolve: {
