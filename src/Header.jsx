9dbfdc8f262c4871758315efee39cc5a33fd74b8
navin <navin@thoghtwin.com>
2023-08-29 15:50:28 +0530

    update toast

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Header = (props) => {
-  const notify = () => toast.success("Welcome user!");
-
-  useEffect(() => {
-    notify();
-  }, [props?.value]);
-
-  return (
-    <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
-      Header Micro-Fronted |
-    </div>
-  );
-};
+    const notify = () => toast.success("Welcome User!");
+    
+    useEffect(() => {
+            notify();
+    }, [props?.value]);
+    
+    return (
+        <div className='p-5 bg-blue-500 text-white text-3xl font-bold'>
+            Header Micro-Fronted |
+            <ToastContainer theme='colored'/>
+        </div>
+    );
+}
 
 export default Header;
